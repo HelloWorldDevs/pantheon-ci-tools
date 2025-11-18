f64070afcc6660814f0166e62058cb84cbd54039
dsatpm <jeremiah@helloworlddevs.com>
2025-11-17 16:39:36 -0800

    update tools to dynamically fetch php version based on project needs

---
+++ files/scripts/get-php-version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Read PHP version from composer.json and select the corresponding Docker image tag.
+# Defaults to 8.1 if not specified.
+PHP_VERSION=$(jq -r '.require.php' composer.json | grep -oE '[0-9]+\.[0-9]+' | head -n 1)
+
+case "$PHP_VERSION" in
+  "8.0")
+    echo "8.x-php8.0"
+    ;;
+  "7.4")
+    echo "8.x-php7.4"
+    ;;
+  *)
+    echo "8.x-php8.1"
+    ;;
+esac
